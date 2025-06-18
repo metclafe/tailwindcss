6bdadc5576412e32d162a1787da0004676c7da4b
metclafe <luesanderdeadman@gmail.com>
2025-06-18 10:53:17 -0300

    Add files via upload

---
+++ tailwindColor.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                'dark-100': '#0f172a',
+                'dark-200': '#1e293b',
+                'dark-300': '#334155'
+            }
+        }
+    }
+}
